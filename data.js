f2ee1fa7c60423d4d5d0c7a381987c4c09509848
Kelvin Sherlock <ksherlock@gmail.com>
2016-08-27 13:18:14 -0400

    add --es2017 and --latest presets.

--- data.js
+++ data.js
@@ -179,6 +179,46 @@ exports.presets = new Map([
 	'transform-exponentiation-operator',
 	]],
 
+	['es2017', [
+	'syntax-trailing-function-commas',
+	'transform-async-to-generator',
+	]],
+
+	// latest is es2015, es2106, es2017.
+	// (todo -- babel latest has options to include/exclude each)
+	['latest', [
+	// es 2015
+	'transform-es2015-template-literals',
+	'transform-es2015-literals',
+	'transform-es2015-function-name',
+	'transform-es2015-arrow-functions',
+	'transform-es2015-block-scoped-functions',
+	'transform-es2015-classes',
+	'transform-es2015-object-super',
+	'transform-es2015-shorthand-properties',
+	'transform-es2015-duplicate-keys',
+	'transform-es2015-computed-properties',
+	'transform-es2015-for-of',
+	'transform-es2015-sticky-regex',
+	'transform-es2015-unicode-regex',
+	'check-es2015-constants',
+	'transform-es2015-spread',
+	'transform-es2015-parameters',
+	'transform-es2015-destructuring',
+	'transform-es2015-block-scoping',
+	'transform-es2015-typeof-symbol',
+	'transform-es2015-modules-commonjs',
+	['transform-regenerator', { async: false, asyncGenerators: false }],
+
+	// es 2016
+	'transform-exponentiation-operator',
+
+	// es 2017
+	'syntax-trailing-function-commas',
+	'transform-async-to-generator',
+	]],
+
+
 	['stage-3', [
 	'syntax-trailing-function-commas',
 	'transform-async-to-generator',
